6adf9c939888f91aab7119f5c7ba9ba479dce713
haisyamm <haisyammaulana22@gmail.com>
2023-12-19 08:33:19 +0700

    Table User

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,13 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Table Users') }}</div>
   
                 <div class="card-body">
-                    <h3>Table Users</h3>
-                    
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Full Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Role</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no = 0; ?>
+                            @foreach($users as $row)
+                            <?php $no++ ?>
+                            <tr>
+                                <th scope="row">{{ $no }}</th>
+                                <td>{{$row->name}}</td>
+                                <td>{{$row->email}}</td>
+                                <td>{{$row->role}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
