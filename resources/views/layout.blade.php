763655fa5fe7f3e2177b76c4b843d2d4163021ea
haisyamm <haisyammaulana22@gmail.com>
2023-12-21 08:45:07 +0700

    Update Name App

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -49,7 +49,7 @@
     
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Laravel</a>
+        <a class="navbar-brand" href="/">SIMARU</a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
